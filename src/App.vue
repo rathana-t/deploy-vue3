cd9b63df4d58ace0c8f06f362746864a623f8bbf
THY RATHANA BE18 - 046 <rathanathy02@gmail.com>
2023-06-15 19:29:00 +0700

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {{ test }}
+  {{ data[0] }}
+  <p class="w-96 bg-green-300">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis laborum distinctio est corrupti voluptatem, perferendis saepe nesciunt explicabo aliquid, fuga accusamus provident tempore reiciendis amet eveniet rem voluptatibus enim! Rem.
+  </p>
 </template>
 
 <script>
@@ -10,7 +15,26 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data(){
+    return {
+      test:"sds",
+      data:[]
+    }
+  },
+
+  // fetch this api https://restcountries.com/v3.1/all
+  mounted(){
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(res => {
+      this.data = res;
+      console.log(res);
+    })
+    .catch(err => console.log(err))
   }
+
 }
 </script>
 
